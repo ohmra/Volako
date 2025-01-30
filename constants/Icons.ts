b27c82fd8e736982627b4849a4f85581d3eb6436
ohmra <rantohary@gmail.com>
2025-01-30 17:33:20 +0300

    added icons

--- constants/Icons.ts
+++ constants/Icons.ts
@@ -6,4 +6,7 @@ export default {
     Balance: require('../assets/icons/Balance.png'),
     Expenses: require('../assets/icons/Expenses.png'),
     Income: require('../assets/icons/Income.png'),
+    Logo: require('../assets/icons/Logo.png'),
+    LeftArrow: require('../assets/icons/LeftArrow.png'),
+    RightArrow: require('../assets/icons/RightArrow.png'),
   };
