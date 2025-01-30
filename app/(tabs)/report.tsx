3b96be890f04e63803a75a831fc45bdc70d0c0c5
ohmra <rantohary@gmail.com>
2025-01-30 19:20:42 +0300

    test

--- app/(tabs)/report.tsx
+++ app/(tabs)/report.tsx
@@ -1,12 +1,75 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image } from "react-native";
+import ThemedText from "@/components/ThemedText";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from 'expo-status-bar';
+import KTextInput from '../../components/KTextInput';
+import CategoryIcons from '@/constants/CategoryIcons'
+import ListItem from '../../components/ListItem';
+import ListGroup from '../../components/ListGroup';
+import Overview from '../../components/Overview'
+import PrimaryButton from '../../components/PrimaryButton';
 
-const Report = () => {
-  return (
-    <View>
-      <Text>Report</Text>
-    </View>
-  )
+type categoryType = keyof typeof CategoryIcons
+
+type ItemType = {
+    category: categoryType,
+    description: string,
+    amount: number,
+    income: boolean
 }
 
-export default Report
+const items: ItemType[] = [
+  {
+    category: "Cafe",
+    description: "Cafe with mom",
+    amount: 400,
+    income: false
+  },
+  {
+    category: "Groceries",
+    description: "Voanjobory tsaramaso",
+    amount: 250,
+    income: false
+  },
+  {
+    category: "Savings",
+    description: "Karama eoah",
+    amount: 1000,
+    income: true
+  },
+  {
+    category: "Savings",
+    description: "Karama eoah",
+    amount: 1000,
+    income: true
+  },
+  {
+    category: "Savings",
+    description: "Karama eoah",
+    amount: 1000,
+    income: true
+  }
+  ]
+
+export default function Index() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ThemedText type="header" color="focused">yes</ThemedText> 
+      <StatusBar backgroundColor="rgba(33, 33, 33, 0.18)" />
+      <KTextInput label="Category name">Groceries</KTextInput>
+      <KTextInput label="Category name" focused>Groceries</KTextInput>
+      <ListItem icon={CategoryIcons.Cafe} category="Cafe" description="Eggs & Veggies" amount={800}/>
+      <ListGroup items={items}/>
+      <Overview />
+      <PrimaryButton icon={CategoryIcons.Cafe} style={{width: 200}}>OK</PrimaryButton>
+    </SafeAreaView>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  }
+})
