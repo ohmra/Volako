54c521b65e8de22bdc6ba51943ae17244baf839a
ohmra <rantohary@gmail.com>
2025-02-02 21:29:12 +0300

    new file

---
+++ components/ListGroupMonthly.tsx
@@ -0,0 +1,77 @@
+import { View, StyleSheet, FlatList, Text } from 'react-native'
+import React, { useState } from 'react'
+import ThemedText from './ThemedText';
+import ListItem from './ListItem';
+import CategoryIcons from '@/constants/CategoryIcons';
+import { useTransaction } from '../hooks/useTransaction';
+
+type categoryType = keyof typeof CategoryIcons
+
+type Transaction = {
+    id: number;
+    amount: number;
+    icon: categoryType;
+    category: string;
+    income: boolean;
+    description: string;
+    created_at: string;
+  };
+
+type ListGroupMonthly = {
+    items: Array<Transaction>,
+    title: string,
+}
+
+const ListGroupMonthly = ({items, title}: ListGroupMonthly) => {
+  const total = useTransaction(items).getTotal();
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <ThemedText type="title" color="darkGray">{title}</ThemedText>
+        <ThemedText type="title" color="darkGray">{total}€</ThemedText>
+      </View>
+      <FlatList style={styles.contentContainer} data={items} renderItem={({item, index}) => {
+            const previousTransactionDate = index > 0 ? items[index-1].created_at : '';
+            return (
+                <>
+                    {(item.created_at !== previousTransactionDate) && 
+                    <Text>{(new Date(item.created_at)).toDateString()}</Text>}
+                    <ListItem icon={CategoryIcons[item.icon]}
+                    category={item.category}
+                    description={item.description}
+                    amount={item.amount}
+                    income={item.income} />
+                </>
+            )
+        }}
+        />
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        padding: 8,
+        borderWidth: 1,
+        borderRadius: 8,
+        borderColor: "#E0E0E0",
+        flex: 1,
+        marginBottom: 12,
+        position: "relative"
+    },
+    titleContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        width: "100%",
+        padding: 8,
+        justifyContent: "space-between"
+    },
+    contentContainer : {
+
+    }
+})
+
+export default ListGroupMonthly
